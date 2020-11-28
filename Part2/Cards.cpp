5146094aef7e866cc366d03a038dcfdc2987a4bd
LettuceFoot <55557605+LettuceFoot@users.noreply.github.com>
2020-11-27 23:58:26 -0500

    Added other constructor reuqired for class, needs more functions

--- Part2/Cards.cpp
+++ Part2/Cards.cpp
@@ -14,19 +14,43 @@ public:
 	const string airlift = "airlift";
 	const string diplomacy = "diplomacy";
 
-	Card() {
-		this.type = "";
-	}
-
-	Card(string cardName) {
-		this.type{ cardName };
-	}
-
+	//Card mrthods need to be here
+	Card();//default
+	Card(string cardName);//simple ciostructor
+	Card(const Card& obj);//copy
+	~Card();//destructor
 private:
-	string type;
+	string* type;
 
 };
 
-//class Deck {
-//
-//};
+//classes functions here
+Card() {
+	this.type = "";
+}
+
+Card(string cardName) {
+	this.type = cardName;
+}
+
+//copy constructor
+Card(const Card& obj) {
+	// body of constructor
+	cout << "copy constructor allocating ptr" << endl;
+	type = new string;
+	*type = *obj.type; // copy the value using deep copy
+}
+
+~Card(void) {
+	cout << "Freeing Card Memory";
+	delete type;
+}
+
+void printCard() {
+	cout << "Here are the cards detail. Card type: " << type << endl;
+
+}
+
+class Deck {
+	//hasmap
+};
